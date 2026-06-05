7d96d84c2a311a9121b3fadde75b455ad5961e38
VitorJacom <vitor.jacom@edu.pucrs.br>
2026-06-05 01:06:50 -0300

    fix: nest basePath inside build config object in tina/config.js

--- tina/config.js
+++ tina/config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   build: {
     outputFolder: "admin",
     publicFolder: "public",
+    basePath: "Site-Instucional-Advocacia-JK",
   },
   media: {
     tina: {
@@ -15,7 +16,6 @@ export default defineConfig({
     },
   },
   publicFolder: "public",
-  basePath: "Site-Instucional-Advocacia-JK",
   schema: {
     collections: [
       {
